52b766d403d76b8e4791e760a6f865113418f43e
jeff.li <lijippy@163.com>
2024-04-25 00:41:28 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# polaris
-tensor search library
+polaris
+========================
+
+the up stream of polaris is based on [faiss](https://github.com/facebookresearch/faiss), and we add some new features to it.
---
+++ polaris/gpu/impl/IVFFlatScan.cuh
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <polaris/Index.h>
+#include <polaris/MetricType.h>
+#include <polaris/gpu/GpuIndicesOptions.h>
+#include <polaris/gpu/impl/GpuScalarQuantizer.cuh>
+#include <polaris/gpu/utils/DeviceVector.cuh>
+#include <polaris/gpu/utils/Tensor.cuh>
+
+namespace faiss {
+namespace gpu {
+
+class GpuResources;
+
+void runIVFFlatScan(
+        Tensor<float, 2, true>& queries,
+        Tensor<idx_t, 2, true>& listIds,
+        DeviceVector<void*>& listData,
+        DeviceVector<void*>& listIndices,
+        IndicesOptions indicesOptions,
+        DeviceVector<idx_t>& listLengths,
+        idx_t maxListLength,
+        int k,
+        faiss::MetricType metric,
+        bool useResidual,
+        Tensor<float, 3, true>& residualBase,
+        GpuScalarQuantizer* scalarQ,
+        // output
+        Tensor<float, 2, true>& outDistances,
+        // output
+        Tensor<idx_t, 2, true>& outIndices,
+        GpuResources* res);
+
+} // namespace gpu
+} // namespace faiss
